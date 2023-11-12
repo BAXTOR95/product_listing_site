56189db3267c24b5843e35893245e778b6cdfb29
Brian Arriaga <brian.arriaga@gmail.com>
2023-11-12 11:28:26 -0500

    Created models ProductCategory and Product

--- store/models.py
+++ store/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='product_images/')
+    average_rating = models.FloatField()
+    rating_count = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title
