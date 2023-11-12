e60cb703af1b9e67256d982388bb4f9087d3e11e
Brian Arriaga <brian.arriaga@gmail.com>
2023-11-12 13:55:59 -0500

    Added view to generate report

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CategoryListView, ProductListView, ProductDetailView
+from .views import CategoryListView, ProductListView, ProductDetailView, ReportView
 
 app_name = 'store'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='category_list'),
     path('categories/<str:category>/', ProductListView.as_view(), name='product_list'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('report/', ReportView.as_view(), name='report_view'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import ListView, DetailView
-from .models import ProductCategory, Product
+from django.views import View
+from django.shortcuts import render
+from .models import ProductCategory, Product, Purchase
+from django.db.models import Sum
+from django.utils.dateparse import parse_date
 
 
 class CategoryListView(ListView):
@@ -18,3 +22,34 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'store/product_detail.html'
+
+
+class ReportView(View):
+    def get(self, request):
+        products = Product.objects.all()
+        return render(request, 'store/report.html', {'products': products})
+
+    def post(self, request):
+        selected_product_id = request.POST.get('product')
+        start_date = parse_date(request.POST.get('start_date'))
+        end_date = parse_date(request.POST.get('end_date'))
+
+        purchases = Purchase.objects.filter(
+            product_id=selected_product_id, purchase_date__range=[start_date, end_date]
+        )
+
+        total_revenue = (
+            purchases.aggregate(Sum('product__price'))['product__price__sum'] or 0
+        )
+        product = Product.objects.get(id=selected_product_id)
+
+        return render(
+            request,
+            'store/report.html',
+            {
+                'total_revenue': total_revenue,
+                'product': product,
+                'purchases': purchases,
+                'products': Product.objects.all(),
+            },
+        )
