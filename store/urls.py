4cdd6c593f440d889afded9c41c508b164b2aa86
Brian Arriaga <brian.arriaga@gmail.com>
2023-11-12 12:45:53 -0500

    Created views and templates for products and categories

---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import CategoryListView, ProductListView
+
+app_name = 'store'
+
+urlpatterns = [
+    path('categories/', CategoryListView.as_views(), name='category_list'),
+    path('categories/<str:category>/', ProductListView.as_view(), name='product_list'),
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import ProductCategory, Product
 
-# Create your views here.
+
+class CategoryListView(ListView):
+    model = ProductCategory
+    template_name = 'store/category_list.html'
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'store/product_list.html'
+
+    def get_queryset(self):
+        return Product.objects.filter(category__tile=self.kwargs['category'])
